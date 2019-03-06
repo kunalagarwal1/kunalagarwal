0d2e9d7cce5b221a3785662fd840e1e3e834aa09
kunalagarwal1 <36959001+kunalagarwal1@users.noreply.github.com>
2019-03-06 22:00:25 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int sum=0,c=0;
+		Scanner sc=new Scanner(System.in);
+ 
+int n=sc.nextInt();
+ 
+ while(n!=0)
+ {
+ 	n=n/10;
+ 	c=c+1;
+ }
+ 
+System.out.println(c);
+	}
+}
